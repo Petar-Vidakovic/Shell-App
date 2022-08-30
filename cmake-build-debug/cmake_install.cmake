d77e4f0e90e5f7c8a999c15a29dc8764dc86a032
Petar-Vidakovic <Petarvidakovic94@gmail.com>
2022-08-30 15:56:24 +1000

    Added more functions and stylized

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/petar/UNI_FOLDER/T2_22/DistSys/milestone1
+# Install script for directory: /Users/petar/UNI_FOLDER/bachCS/YEAR2/T2_2022/DistSys/milestone1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/petar/UNI_FOLDER/T2_22/DistSys/milestone1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/petar/UNI_FOLDER/bachCS/YEAR2/T2_2022/DistSys/milestone1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -17,358 +17,365 @@ void systemInfo(void);
 void localTime(void);
 void currentPath(void);
 void systemMsg(void);
-void get(char *fileName);
-int isOperator(const char *operator);
-void put(char **tokenContainer, int inputSize);
+void get(char* fileName);
+int isOperator(const char* operator);
+void put(char** tokenContainer, int inputSize);
 void push(int num);
 int pop();
-int deleteSubDirs(const char *path, const struct stat *sb, int t, struct FTW *ftw);
-void prefix(char *const *tokenContainer, char *exp, int inputSize);
-void tokenizeInput(char *input, char *token, char **tokenContainer);
+int deleteSubDirs(const char* path, const struct stat* sb, int t, struct FTW* ftw);
+void prefix(char* const* tokenContainer, char* exp, int inputSize);
+void tokenizeInput(char* input, char* token, char** tokenContainer);
 
 int main()
 {
-    char *options[] = {"calc", "time", "path", "sys", "put", "get"};
-    char input[SIZE] = {};
-    char *token = NULL;
-    char *tokenContainer[SIZE] = {};
-    char expression[SIZE] = {};
-
-    printf("Welcome to the shell.\n");
-    systemMsg();
-
-    while(strcmp(input, "quit") != 0)
-    {
-        bzero(input, SIZE);
-        printf("> ");
-        fgets(input, SIZE, stdin);
-
-        // ensure single inputs works
-        input[strcspn(input, "\n")] = 0;
-
-        // tokenize string and fill token array
-        tokenizeInput(input, token, tokenContainer);
-
-        int inputSize = 1;
-        size_t optLen = sizeof(options)/sizeof(options[0]);
-
-        // find the number of items in token array
-        while(tokenContainer[inputSize] != NULL){inputSize++;}
-
-        int n = 0;
-        for(int i = 0; i < optLen; i++)
-        {
-            if(strcmp(tokenContainer[0], options[i]) == 0)
-            {
-                switch(i)
-                {
-                    case 0:
-                        prefix(tokenContainer, expression, inputSize);
-                        break;
-
-                    case 1:
-                        if(inputSize >= 2)
-                        {
-                            printf("ERROR.\n");
-                        }
-                        else {localTime();}
-                        break;
-
-                    case 2:
-                        if(inputSize >= 2)
-                        {
-                            printf("ERROR.\n");
-                        }
-                        else {currentPath();}
-                        break;
-
-                    case 3:
-                        if(inputSize >= 2)
-                        {
-                            printf("ERROR.\n");
-                        }
-                        else {systemInfo();}
-                        break;
-
-                    case 4:
-                        put(tokenContainer, inputSize);
-                        break;
-
-                    case 5:
-                        get(tokenContainer[1]);
-                        break;
-
-                    default:
-                        exit(-1);
-                }
-            }
-            else{n++;}
-
-            if((n == 6) && strcmp(tokenContainer[0], "quit") != 0)
-            {
-                printf("ERROR.\n");
-                break;
-            }
-        }
-        //systemMsg();
-        bzero(expression, SIZE);
-        bzero(tokenContainer, SIZE);
-        bzero(stack, SIZE);
-    }
-    printf("Program quit.\n");
+	char* options[] = {"calc", "time", "path", "sys", "put", "get"};
+	char input[SIZE] = {};
+	char* token = NULL;
+	char* tokenContainer[SIZE] = {};
+	char expression[SIZE] = {};
+	
+	printf("Welcome to the shell.\n");
+	systemMsg();
+	
+	while(strcmp(input, "quit") != 0)
+	{
+		bzero(input, SIZE);
+		printf("> ");
+		fgets(input, SIZE, stdin);
+		
+		// ensure single inputs works
+		input[strcspn(input, "\n")] = 0;
+		
+		// tokenize string and fill token array
+		tokenizeInput(input, token, tokenContainer);
+		
+		int inputSize = 1;
+		size_t optLen = sizeof(options) / sizeof(options[0]);
+		
+		// find the number of items in token array
+		while(tokenContainer[inputSize] != NULL)
+		{inputSize++;}
+		
+		int n = 0;
+		for(int i = 0; i < optLen; i++)
+		{
+			if(strcmp(tokenContainer[0], options[i]) == 0)
+			{
+				switch(i)
+				{
+				case 0: prefix(tokenContainer, expression, inputSize);
+					break;
+				
+				case 1:
+					if(inputSize >= 2)
+					{
+						printf("ERROR.\n");
+					}
+					else
+					{localTime();}
+					break;
+				
+				case 2:
+					if(inputSize >= 2)
+					{
+						printf("ERROR.\n");
+					}
+					else
+					{currentPath();}
+					break;
+				
+				case 3:
+					if(inputSize >= 2)
+					{
+						printf("ERROR.\n");
+					}
+					else
+					{systemInfo();}
+					break;
+				
+				case 4: put(tokenContainer, inputSize);
+					break;
+				
+				case 5: get(tokenContainer[1]);
+					break;
+				
+				default: exit(-1);
+				}
+			}
+			else
+			{n++;}
+			
+			if((n == 6) && strcmp(tokenContainer[0], "quit") != 0)
+			{
+				printf("ERROR.\n");
+				break;
+			}
+		}
+		//systemMsg();
+		bzero(expression, SIZE);
+		bzero(tokenContainer, SIZE);
+		bzero(stack, SIZE);
+	}
+	printf("Program quit.\n");
 }
 
-void tokenizeInput(char *input, char *token, char **tokenContainer) {
-    token = strtok(input, " ");
-    int k = 0;
-    while(token != NULL)
-    {
-        //printf("%s", token);
-        tokenContainer[k] = token;
-        token = strtok(NULL, " ");
-        k++;
-    }
-}
-int deleteSubDirs(const char *path, const struct stat *sb, int t, struct FTW *ftw)
+void tokenizeInput(char* input, char* token, char** tokenContainer)
 {
-    return remove(path);
+	token = strtok(input, " ");
+	int k = 0;
+	while(token != NULL)
+	{
+		//printf("%s", token);
+		tokenContainer[k] = token;
+		token = strtok(NULL, " ");
+		k++;
+	}
 }
-void put(char **tokenContainer, int inputSize)
+int deleteSubDirs(const char* path, const struct stat* sb, int t, struct FTW* ftw)
 {
-    char path[1024] = {};
-    getcwd(path, sizeof(path));
-
-    // append the new file name
-    strcat(path, "/");
-    strcat(path, tokenContainer[1]);
-
-    if(strcmp(tokenContainer[inputSize - 1], "-f") == 0)
-    {
-        // -f
-        tokenContainer[inputSize - 1] = NULL;
-        nftw(path, deleteSubDirs, 10, FTW_DEPTH|FTW_MOUNT|FTW_PHYS);
-        mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO);
-
-        // add files
-        int j = 2;
-        while(tokenContainer[j] != NULL)
-        {
-            strcat(path, "/");
-            strcat(path, tokenContainer[j]);
-
-            fclose(fopen(path, "a"));
-            j++;
-            bzero(path, sizeof(path));
-            getcwd(path, sizeof(path));
-            strcat(path, "/");
-            strcat(path, tokenContainer[1]);
-        }
-    }
-    else
-    {
-        // no -f - returns error
-        if (mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO) == -1){
-            printf("Error: %s\n", strerror(errno));
-        }
-
-        // add files
-        int j = 2;
-        while(tokenContainer[j] != NULL)
-        {
-            strcat(path, "/");
-            strcat(path, tokenContainer[j]);
-
-            fclose(fopen(path, "a"));
-            j++;
-            bzero(path, sizeof(path));
-            getcwd(path, sizeof(path));
-            strcat(path, "/");
-            strcat(path, tokenContainer[1]);
-        }
-    }
+	return remove(path);
+}
+void put(char** tokenContainer, int inputSize)
+{
+	char path[1024] = {};
+	getcwd(path, sizeof(path));
+	
+	// append the new file name
+	strcat(path, "/");
+	strcat(path, tokenContainer[1]);
+	
+	if(strcmp(tokenContainer[inputSize - 1], "-f") == 0)
+	{
+		// -f
+		tokenContainer[inputSize - 1] = NULL;
+		nftw(path, deleteSubDirs, 10, FTW_DEPTH | FTW_MOUNT | FTW_PHYS);
+		mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO);
+		
+		// add files
+		int j = 2;
+		while(tokenContainer[j] != NULL)
+		{
+			strcat(path, "/");
+			strcat(path, tokenContainer[j]);
+			
+			fclose(fopen(path, "a"));
+			j++;
+			bzero(path, sizeof(path));
+			getcwd(path, sizeof(path));
+			strcat(path, "/");
+			strcat(path, tokenContainer[1]);
+		}
+	}
+	else
+	{
+		// no -f - returns error
+		if(mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO) == -1)
+		{
+			printf("Error: %s\n", strerror(errno));
+		}
+		
+		// add files
+		int j = 2;
+		while(tokenContainer[j] != NULL)
+		{
+			strcat(path, "/");
+			strcat(path, tokenContainer[j]);
+			
+			fclose(fopen(path, "a"));
+			j++;
+			bzero(path, sizeof(path));
+			getcwd(path, sizeof(path));
+			strcat(path, "/");
+			strcat(path, tokenContainer[1]);
+		}
+	}
 }
 void systemInfo(void)
 {
-    struct utsname uts;
-    uname(&uts);
-    printf("System name - %s\n", uts.sysname);
-    printf("Version     - %s \n", uts.version);
-    printf("Hardware    - %s \n", uts.machine);
+	struct utsname uts;
+	uname(&uts);
+	printf("System name - %s\n", uts.sysname);
+	printf("Version     - %s \n", uts.version);
+	printf("Hardware    - %s \n", uts.machine);
 }
 void localTime(void)
 {
-    time_t t;
-    struct tm *ptr;
-    char arr[64];
-
-    time(&t);
-    ptr = localtime(&t);
-    if(strftime(arr, 64, "Time and Date: %r - %d/%b/%Y", ptr) != 0){
-        printf("%s\n", arr);
-    }
+	time_t t;
+	struct tm* ptr;
+	char arr[64];
+	
+	time(&t);
+	ptr = localtime(&t);
+	if(strftime(arr, 64, "Time and Date: %r - %d/%b/%Y", ptr) != 0)
+	{
+		printf("%s\n", arr);
+	}
 }
-void currentPath(void){
-    char p1[1024] = {};
-    if(getcwd(p1, sizeof(p1)) != NULL)
-        printf("%s\n", p1);
-}
-void systemMsg(void){
-    printf("Input a command:\n");
-    printf("- calc <prefix exp>\n- time\n- path\n- sys\n- put <dirname> <filenames> <[-f]>\n- get filename\n- quit\n");
-}
-void get(char *fileName)
+void currentPath(void)
 {
-    char io[6];
-    FILE *fp = NULL;
-    char *line = NULL;
-    size_t size = 0;
-    int lineNum = 0, k = 0;
-    char pathStr[1024] = {};
-
-    getcwd(pathStr, sizeof(pathStr));
-    strcat(pathStr, "/");
-    strcat(pathStr, fileName);
-
-    fp = fopen(fileName, "r");
-
-    here:
-    while(getline(&line, &size, fp) != -1)
-    {
-        printf("%d:%s", lineNum, line);
-        lineNum++, k++;
-
-        if(k == 40)
-        {
-            printf("<Press any key to continue>\n");
-            fgets(io, 6, stdin);
-            k = 0;
-            goto here;
-        }
-    }
-    printf("<EOF reached>\n\n");
-    line = NULL;
-    fclose(fp);
-    systemMsg();
+	char p1[1024] = {};
+	if(getcwd(p1, sizeof(p1)) != NULL)
+		printf("%s\n", p1);
+}
+void systemMsg(void)
+{
+	printf("Input a command:\n");
+	printf("- calc <prefix exp>\n- time\n- path\n- sys\n- put <dirname> <filenames> <[-f]>\n- get filename\n- quit\n");
+}
+void get(char* fileName)
+{
+	char io[6];
+	FILE* fp = NULL;
+	char* line = NULL;
+	size_t size = 0;
+	int lineNum = 0, k = 0;
+	char pathStr[1024] = {};
+	
+	getcwd(pathStr, sizeof(pathStr));
+	strcat(pathStr, "/");
+	strcat(pathStr, fileName);
+	
+	fp = fopen(fileName, "r");
+	
+	here:
+	while(getline(&line, &size, fp) != -1)
+	{
+		printf("%d:%s", lineNum, line);
+		lineNum++, k++;
+		
+		if(k == 40)
+		{
+			printf("<Press any key to continue>\n");
+			fgets(io, 6, stdin);
+			k = 0;
+			goto here;
+		}
+	}
+	printf("<EOF reached>\n\n");
+	line = NULL;
+	fclose(fp);
+	systemMsg();
 }
 void push(int num)
 {
-    top++;
-    stack[top] = num;
+	top++;
+	stack[top] = num;
 }
 int pop()
 {
-    int num;
-    num = stack[top];
-    top--;
-    return num;
+	int num;
+	num = stack[top];
+	top--;
+	return num;
 }
-int isOperator(const char *operator)
+int isOperator(const char* operator)
 {
-    if (*operator == '+'){return 1;}
-    if (*operator == '-'){return 2;}
-    if (*operator == '*'){return 3;}
-    if (*operator == '/'){return 4;}
-    return -1;
+	if(*operator == '+') return 1;
+	if(*operator == '-') return 2;
+	if(*operator == '*') return 3;
+	if(*operator == '/') return 4;
+	return -1;
 }
-void prefix(char *const *tokenContainer, char *exp, int inputSize) {
-    int num1 = 0, num2 = 0, operator = 0, r = 0;
-    if(inputSize > 2)
-    {
-        // prefix eval on expression with spaces
-        while(inputSize --> 1) {
-            if (isnumber(*tokenContainer[inputSize]) == 0) {
-                // operator
-                operator = isOperator(tokenContainer[inputSize]);
-
-                // pop 2 do eval push to stack
-                num1 = pop();
-                num2 = pop();
-
-                switch (operator) {
-                    case 1:
-                        // add
-                        r = num2 + num1;
-                        printf("%d + %d = %d\n", num2, num1, r);
-                        break;
-                    case 2:
-                        // sub
-                        r = num2 - num1;
-                        printf("%d - %d = %d\n", num2, num1, r);
-                        break;
-                    case 3:
-                        // multi
-                        r = num2 * num1;
-                        printf("%d * %d = %d\n", num2, num1, r);
-                        break;
-                    case 4:
-                        // div
-                        r = num2 / num1;
-                        printf("%d / %d = %d\n", num2, num1, r);
-                        break;
-                    default:
-                        exit(1);
-                }
-                push(r);
-            }
-            else
-            {
-                // number
-                int num = atoi(tokenContainer[inputSize]);
-                push(num);
-            }
-        }
-        printf("result:%d\n", stack[top]);
-    }
-    else
-    {
-        // expression is all 1 string
-        strcpy(exp, tokenContainer[1]);
-        size_t expLen = strlen(exp);
-        while(expLen --> 0)
-        {
-            if(isnumber(exp[expLen]) == 0)
-            {
-                // operator
-                operator = isOperator(&exp[expLen]);
-
-                // pop 2 do eval push to stack
-                num1 = pop();
-                num2 = pop();
-
-                switch(operator)
-                {
-                    case 1:
-                        // add
-                        r = num2 + num1;
-                        printf("%d + %d = %d\n", num2, num1, r);
-                        break;
-                    case 2:
-                        // sub
-                        r = num2 - num1;
-                        printf("%d - %d = %d\n", num2, num1, r);
-                        break;
-                    case 3:
-                        // mult
-                        r = num2 * num1;
-                        printf("%d * %d = %d\n", num2, num1, r);
-                        break;
-                    case 4:
-                        // div
-                        r = num2 / num1;
-                        printf("%d / %d = %d\n", num2, num1, r);
-                        break;
-                    default:
-                        exit(1);
-                }
-                push(r);
-            }
-            else
-            {
-                // number
-                int num = (exp[expLen] - '0');
-                push(num);
-            }
-        }
-        printf("result:%d\n", stack[top]);
-    }
+void prefix(char* const* tokenContainer, char* exp, int inputSize)
+{
+	int num1 = 0, num2 = 0, operator = 0, r = 0;
+	if(inputSize > 2)
+	{
+		// prefix eval on expression with spaces
+		while(inputSize --> 1)
+		{
+			if(isnumber(*tokenContainer[inputSize]) == 0)
+			{
+				// operator
+				operator = isOperator(tokenContainer[inputSize]);
+				
+				// pop 2 do eval push to stack
+				num1 = pop();
+				num2 = pop();
+				
+				switch(operator)
+				{
+				case 1:
+					// add
+					r = num2 + num1;
+					printf("%d + %d = %d\n", num2, num1, r);
+					break;
+				case 2:
+					// sub
+					r = num2 - num1;
+					printf("%d - %d = %d\n", num2, num1, r);
+					break;
+				case 3:
+					// multi
+					r = num2 * num1;
+					printf("%d * %d = %d\n", num2, num1, r);
+					break;
+				case 4:
+					// div
+					r = num2 / num1;
+					printf("%d / %d = %d\n", num2, num1, r);
+					break;
+				default: exit(1);
+				}
+				push(r);
+			}
+			else
+			{
+				// number
+				int num = atoi(tokenContainer[inputSize]);
+				push(num);
+			}
+		}
+		printf("result:%d\n", stack[top]);
+	}
+	else
+	{
+		// expression is all 1 string
+		strcpy(exp, tokenContainer[1]);
+		size_t expLen = strlen(exp);
+		while(expLen-- > 0)
+		{
+			if(isnumber(exp[expLen]) == 0)
+			{
+				operator = isOperator(&exp[expLen]);
+				
+				// pop 2 do eval push to stack
+				num1 = pop();
+				num2 = pop();
+				
+				switch(operator)
+				{
+				case 1:
+					// add
+					r = num2 + num1;
+					printf("%d + %d = %d\n", num2, num1, r);
+					break;
+				case 2:
+					// sub
+					r = num2 - num1;
+					printf("%d - %d = %d\n", num2, num1, r);
+					break;
+				case 3:
+					// multi
+					r = num2 * num1;
+					printf("%d * %d = %d\n", num2, num1, r);
+					break;
+				case 4:
+					// div
+					r = num2 / num1;
+					printf("%d / %d = %d\n", num2, num1, r);
+					break;
+				default: exit(1);
+				}
+				push(r);
+			}
+			else
+			{
+				// number
+				int num = (exp[expLen] - '0');
+				push(num);
+			}
+		}
+		printf("result:%d\n", stack[top]);
+	}
 }
